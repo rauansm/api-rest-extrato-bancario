135623213bcdba3083f39ae237c4975bea43d332
Rauan Santos <rauanmor@gmail.com>
2024-02-11 07:12:07 -0300

    cria tabela transferencia no mysql usando flyway

---
+++ src/main/resources/db/migration/V02__cria-tabela-transferencia.sql
@@ -0,0 +1,16 @@
+create table transferencia (
+id_tranferencia bigint not null auto_increment,
+data_transferencia timestamp not null,
+valor numeric (20,2) not null,
+tipo varchar(15) not null,
+nome_operador_transacao varchar(50) not null,
+conta_id bigint not null,
+
+primary key (id_tranferencia),
+key conta_id_idx (conta_id),
+constraint fk_conta foreign key (conta_id) references conta (id_conta)
+) engine=InnoDB default charset=utf8;
+
+
+
+
